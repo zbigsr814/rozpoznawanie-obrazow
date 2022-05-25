df6ab8352ce570ec1af558e0a3e5e7e3fa214c21
Zbigniew Sroczyński <zbigsro814@student.polsl.pl>
2022-05-25 17:12:46 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace okrag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnZaladuj = new System.Windows.Forms.Button();
+            this.btnSprOkrag = new System.Windows.Forms.Button();
+            this.btnFProg = new System.Windows.Forms.Button();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSzary = new System.Windows.Forms.Button();
+            this.btnRozmarz = new System.Windows.Forms.Button();
+            this.btnWyzSr = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(331, 87);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(336, 246);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // btnZaladuj
+            // 
+            this.btnZaladuj.Location = new System.Drawing.Point(47, 27);
+            this.btnZaladuj.Name = "btnZaladuj";
+            this.btnZaladuj.Size = new System.Drawing.Size(125, 24);
+            this.btnZaladuj.TabIndex = 1;
+            this.btnZaladuj.Text = "Załaduj obraz";
+            this.btnZaladuj.UseVisualStyleBackColor = true;
+            this.btnZaladuj.Click += new System.EventHandler(this.btnZaladuj_Click);
+            // 
+            // btnSprOkrag
+            // 
+            this.btnSprOkrag.Location = new System.Drawing.Point(47, 175);
+            this.btnSprOkrag.Name = "btnSprOkrag";
+            this.btnSprOkrag.Size = new System.Drawing.Size(125, 24);
+            this.btnSprOkrag.TabIndex = 2;
+            this.btnSprOkrag.Text = "Sprawdź okrągłość";
+            this.btnSprOkrag.UseVisualStyleBackColor = true;
+            this.btnSprOkrag.Click += new System.EventHandler(this.btnSprOkrag_Click);
+            // 
+            // btnFProg
+            // 
+            this.btnFProg.Location = new System.Drawing.Point(47, 116);
+            this.btnFProg.Name = "btnFProg";
+            this.btnFProg.Size = new System.Drawing.Size(125, 24);
+            this.btnFProg.TabIndex = 3;
+            this.btnFProg.Text = "Filtr progowy";
+            this.btnFProg.UseVisualStyleBackColor = true;
+            this.btnFProg.Click += new System.EventHandler(this.btnFProg_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(582, 12);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 20);
+            this.txtX.TabIndex = 4;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(582, 38);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 20);
+            this.txtY.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(521, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Pozycja x:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(521, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Pozycja y:";
+            // 
+            // btnSzary
+            // 
+            this.btnSzary.Location = new System.Drawing.Point(47, 57);
+            this.btnSzary.Name = "btnSzary";
+            this.btnSzary.Size = new System.Drawing.Size(125, 24);
+            this.btnSzary.TabIndex = 8;
+            this.btnSzary.Text = "Utwórz szary";
+            this.btnSzary.UseVisualStyleBackColor = true;
+            this.btnSzary.Click += new System.EventHandler(this.btnSzary_Click);
+            // 
+            // btnRozmarz
+            // 
+            this.btnRozmarz.Location = new System.Drawing.Point(47, 87);
+            this.btnRozmarz.Name = "btnRozmarz";
+            this.btnRozmarz.Size = new System.Drawing.Size(125, 23);
+            this.btnRozmarz.TabIndex = 9;
+            this.btnRozmarz.Text = "Rozmarz obraz";
+            this.btnRozmarz.UseVisualStyleBackColor = true;
+            this.btnRozmarz.Click += new System.EventHandler(this.btnRozmarz_Click);
+            // 
+            // btnWyzSr
+            // 
+            this.btnWyzSr.Location = new System.Drawing.Point(47, 146);
+            this.btnWyzSr.Name = "btnWyzSr";
+            this.btnWyzSr.Size = new System.Drawing.Size(136, 23);
+            this.btnWyzSr.TabIndex = 10;
+            this.btnWyzSr.Text = "Wyznacz środki figur";
+            this.btnWyzSr.UseVisualStyleBackColor = true;
+            this.btnWyzSr.Click += new System.EventHandler(this.btnWyzSr_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnWyzSr);
+            this.Controls.Add(this.btnRozmarz);
+            this.Controls.Add(this.btnSzary);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.btnFProg);
+            this.Controls.Add(this.btnSprOkrag);
+            this.Controls.Add(this.btnZaladuj);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnZaladuj;
+        private System.Windows.Forms.Button btnSprOkrag;
+        private System.Windows.Forms.Button btnFProg;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSzary;
+        private System.Windows.Forms.Button btnRozmarz;
+        private System.Windows.Forms.Button btnWyzSr;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace okrag
+{
+    public partial class Form1 : Form
+    {
+        Bitmap btm;
+        List<Punkt> pktSrodkowe = new List<Punkt>();
+        bool[,] tabOkr;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnZaladuj_Click(object sender, EventArgs e)
+        {
+            btm = new Bitmap(@"..\..\obrazy\koll.jpg");
+            btm = CreateNonIndexedImage(btm);
+            pictureBox1.Width = btm.Width;
+            pictureBox1.Height = btm.Height;
+            pictureBox1.Image = btm;
+        }
+
+        public Bitmap CreateNonIndexedImage(Bitmap src)
+        {
+            Bitmap newBmp = new Bitmap(src.Width, src.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics gfx = Graphics.FromImage(newBmp))
+            {
+                gfx.DrawImage(src, 0, 0);
+            }
+
+            return newBmp;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            txtX.Text = (Cursor.Position.X-331).ToString();
+            txtY.Text = (Cursor.Position.Y-110).ToString();
+        }
+
+        private void btnFProg_Click(object sender, EventArgs e)
+        {
+            Color clr;
+            int r;
+
+            for (int i = 0; i < btm.Width; i++)
+            {
+                for (int j = 0; j < btm.Height; j++)
+                {
+                    clr = btm.GetPixel(i, j);
+                    r = clr.R;
+
+                    if(r<40) btm.SetPixel(i, j, Color.Black);
+                    else btm.SetPixel(i, j, Color.White);
+                    
+                }
+            }
+            pictureBox1.Image = btm;
+        }
+
+        private void btnSzary_Click(object sender, EventArgs e)
+        {
+            Color clr;
+            int r, g, b, mix;
+            for (int i = 0; i < btm.Width; i++)
+            {
+                for (int j = 0; j < btm.Height; j++)
+                {
+                    clr = btm.GetPixel(i, j);
+                    r = clr.R;
+                    g = clr.G;
+                    b = clr.B;
+                    mix = (r + g + b) / 3;
+                    btm.SetPixel(i, j, Color.FromArgb(mix,mix,mix));
+                }
+            }
+            pictureBox1.Image = btm;
+        }
+
+        private void btnRozmarz_Click(object sender, EventArgs e)
+        {
+            long mixR = 0;
+            long mix = 0;
+            int poziom = 5;
+            Color clr;
+
+            for (int i = 0; i < btm.Width; i++)
+            {
+                for (int j = 0; j < btm.Height; j++)
+                {
+                    mixR = 0;
+                    mix = 0;
+
+                    for (int k = -poziom / 2; k < poziom / 2; k++)
+                    {
+
+                        for (int l = -poziom / 2; l < poziom / 2; l++)
+                        {
+                            if (i + k > 0 && i + k < btm.Width && j + l > 0 && j + l < btm.Height)
+                            {
+                                clr = btm.GetPixel(i + k, j + l);
+
+                                mixR = +clr.R;
+                                mix = mix + mixR;
+                            }
+                        }
+                    }
+                    int srR = Convert.ToInt16(mix / (poziom * poziom));
+                    btm.SetPixel(i, j, Color.FromArgb(srR, srR, srR));
+                }
+            }
+            pictureBox1.Image = btm;
+        }
+
+        private void btnWyzSr_Click(object sender, EventArgs e)
+        {
+            int btmX, btmY;
+            btmX = btm.Width;
+            btmY = btm.Height;
+            tabOkr = new bool[btmX, btmY];
+
+            Color clr;
+
+            for (int i = 0; i < btm.Width; i++)
+            {
+                for (int j = 0; j < btm.Height; j++)
+                {
+                    clr = btm.GetPixel(i, j);
+                    if (clr.R > 200) tabOkr[i, j] = false;
+                    else if (clr.R < 200) tabOkr[i, j] = true;
+                }
+            }
+
+            int objXpocz = 0, objYpocz = 0;
+            int objXkon = 0, objYkon = 0;
+            bool jestObiekt = false;
+            int cnt = 0;
+            List<Punkt> srednia = new List<Punkt>();
+            srednia.Clear();
+
+            for (int i = 0; i < btmX; i++)
+            {
+                for (int j = 0; j < btmY; j++)
+                {
+                    if (jestObiekt == false && tabOkr[i, j] == true)
+                    {
+                        jestObiekt = true;
+                        objXpocz = j;
+                    }
+                    if(jestObiekt==true && tabOkr[i,j]==false)
+                    {
+                        jestObiekt = false;
+                        objXkon = j;
+                        btm.SetPixel(i, (objXkon - objXpocz) / 2 + objXpocz, Color.Red);
+                        srednia.Add(new Punkt(i, (objXkon - objXpocz) / 2 + objXpocz));
+                    }
+                }
+            }
+            pktSrodkowe.Clear();
+            for (int j = 0; j < btmY; j++)
+            {
+                for (int i = 0; i < btmX; i++)
+                {
+                    if (jestObiekt == false && tabOkr[i, j] == true)
+                    {
+                        jestObiekt = true;
+                        objXpocz = i;
+
+                    }
+                    else if (jestObiekt == true && tabOkr[i, j] == false)
+                    {
+                        jestObiekt = false;
+                        objXkon = i;
+                        btm.SetPixel((objXpocz + objXkon) / 2, j, Color.Red);
+                        foreach (var pkt in srednia)
+                        {
+                            if((objXpocz + objXkon) / 2 == pkt.x && j == pkt.y)
+                            {
+                                pktSrodkowe.Add(new Punkt(pkt.x, pkt.y));
+                                punkt(pkt.x, pkt.y, Color.Yellow, 5);
+                            }
+                        }
+                    }
+                }
+            }
+
+            pictureBox1.Image = btm;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int y = Cursor.Position.Y-110;
+            int x = Cursor.Position.X-331;
+
+            punkt(x, y);
+            pictureBox1.Image = btm;
+            int cntP = 0, cntD = 0, cntL = 0, cntG = 0;
+
+            while (tabOkr[x+cntP, y] == true) cntP++;
+            while (tabOkr[x, y+ cntD] == true) cntD++;
+            while (tabOkr[x - cntL, y] == true) cntL++;
+            while (tabOkr[x, y - cntG] == true) cntG++;
+            MessageBox.Show(cntP.ToString() + "--" + cntD.ToString() + "  " + cntL.ToString() + "--" + cntG.ToString());
+        }
+
+        void prostaPozioma(int x, int y, int dlugoscX)
+        {
+            for (int i = -dlugoscX/2; i < dlugoscX/2; i++)
+            {
+                if(x + i >= 0 && x + i < btm.Width)
+                {
+                    btm.SetPixel(x + i, y, Color.Red);
+                }
+            }
+        }
+
+        void prostaPozioma(int x, int y)
+        {
+            for (int i = 0; i < btm.Width; i++)
+            {
+                 btm.SetPixel(i, y, Color.Red);
+            }
+        }
+
+        void punkt(int x, int y)
+        {
+            int rozmiar = 5;
+
+            for (int j = -rozmiar/2; j < rozmiar/2; j++)
+            {
+                for (int k = -rozmiar / 2; k < rozmiar; k++)
+                {
+                    if (x + j > 0 && x + j < btm.Width && y + k > 0 && y + k < btm.Height)
+                    {
+                        btm.SetPixel(x + j, y + k, Color.Red);
+                    }
+                        
+                }
+            }
+        }
+
+        void punkt(int x, int y, Color clr)
+        {
+            int rozmiar = 5;
+
+            for (int j = -rozmiar / 2; j < rozmiar / 2; j++)
+            {
+                for (int k = -rozmiar / 2; k < rozmiar; k++)
+                {
+                    if (x + j > 0 && x + j < btm.Width && y + k > 0 && y + k < btm.Height)
+                    {
+                        btm.SetPixel(x + j, y + k, clr);
+                    }
+
+                }
+            }
+        }
+
+        void punkt(int x, int y, Color clr, int rozmiar)
+        {
+            for (int j = -rozmiar / 2; j < rozmiar / 2; j++)
+            {
+                for (int k = -rozmiar / 2; k < rozmiar; k++)
+                {
+                    if (x + j > 0 && x + j < btm.Width && y + k > 0 && y + k < btm.Height)
+                    {
+                        btm.SetPixel(x + j, y + k, clr);
+                    }
+
+                }
+            }
+        }
+
+        public class Punkt
+        {
+            public int x;
+            public int y;
+
+            public Punkt(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public void NadpiszPkt(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        void RysPix(int x, int y)
+        {
+            if (x > 0 && x < btm.Width && y > 0 && y < btm.Height)
+                btm.SetPixel(x,y, Color.Orange);
+        }
+
+        private void btnSprOkrag_Click(object sender, EventArgs e)
+        {
+            bool znalezionoFigure = false;
+            foreach (var pkt in pktSrodkowe)
+            {
+                int cntP = 0, cntD = 0, cntL = 0, cntG = 0;
+                int cntGP = 0, cntGL = 0, cntDP = 0, cntDL = 0;
+
+                try
+                {
+                    while (tabOkr[pkt.x + cntP, pkt.y] == true) { cntP++; }//RysPix(pkt.x + cntP, pkt.y); }
+                    while (tabOkr[pkt.x, pkt.y + cntD] == true) { cntD++; }//RysPix(pkt.x, pkt.y + cntD); }
+                    while (tabOkr[pkt.x - cntL, pkt.y] == true) { cntL++; }//RysPix(pkt.x - cntL, pkt.y); }
+                    while (tabOkr[pkt.x, pkt.y - cntG] == true) { cntG++; }//RysPix(pkt.x, pkt.y - cntG); }
+
+                    while (tabOkr[pkt.x + cntDP, pkt.y + cntDP] == true) { cntDP++; }//RysPix(pkt.x + cntDP, pkt.y + cntDP); }
+                    while (tabOkr[pkt.x - cntGL, pkt.y - cntGL] == true) { cntGL++; }//RysPix(pkt.x - cntGL, pkt.y - cntGL); }
+                    while (tabOkr[pkt.x + cntGP, pkt.y - cntGP] == true) { cntGP++; }//RysPix(pkt.x + cntGP, pkt.y - cntGP); }
+                    while (tabOkr[pkt.x - cntDL, pkt.y + cntDL] == true) { cntDL++; }//RysPix(pkt.x - cntDL, pkt.y + cntDL); }
+
+                    int procentTolerancji = 14;
+                    int parKola = (procentTolerancji * cntP) / 100;
+
+                    int[] promienie = new int[8] { cntD, cntL, cntG, cntP, cntGL, cntGP, cntDL, cntDP };
+                    Odchylenie odch = new Odchylenie(promienie);
+
+                    if (cntD >= cntG - parKola && cntD <= cntG + parKola
+                        && cntD >= cntL - parKola && cntD <= cntL + parKola
+                        && cntD >= cntP - parKola && cntD <= cntP + parKola)
+                    {
+                        if (cntGP >= cntGL - parKola && cntGP <= cntGL + parKola
+                        && cntGP >= cntDP - parKola && cntGP <= cntDP + parKola
+                        && cntGP >= cntDL - parKola && cntGP <= cntDL + parKola)
+                        {
+                            punkt(pkt.x, pkt.y, Color.Red, 5);
+                            znalezionoFigure = true;
+                            MessageBox.Show("Wykryto koło\n" +
+                                "Promień uśredniony: " + odch.srednia.ToString() + "pix\n" +
+                                "Odchylenie: " + odch.odchylenie.ToString());
+                        }
+                    }
+                }
+                catch(IndexOutOfRangeException)
+                {
+
+                }
+                pictureBox1.Image = btm;
+            }
+            if (znalezionoFigure == false) MessageBox.Show("Nie znaleziono żadnych figur geometrycznych");
+        }
+
+        class Odchylenie
+        {
+            public double srednia = 0;
+            public double odchylenie = 0;
+
+            public Odchylenie(int[] promienie)
+            {
+                double suma = 0;
+                foreach (int promien in promienie)
+                {
+                    suma += promien;
+                }
+                srednia = suma / promienie.Length;
+                suma = 0;
+
+                foreach (double promien in promienie)
+                {
+                    suma += ((promien-srednia) * (promien-srednia));
+                }
+                odchylenie = Math.Sqrt(suma / promienie.Length);
+            }
+        }
+    }
+}
